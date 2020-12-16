da1621c61708ffa86974438f075d6fcd23c452d0
Riguja89 <jaimegutierrios@gmail.com>
2020-12-16 00:24:27 -0500

    v1.o

---
+++ src/components/Crear.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="Crear">
+    <form action="Crear festividad">
+      <label>Fecha de Inicio:</label>
+      <input v-model="FechaIni" id="FechaIni" type="date"/>
+      <br />
+      <label>Fecha de fin:</label>
+      <input v-model="FechaFin" type="date" />
+      <br />
+      <label>Ciudad:</label>
+      <input v-model="Ciudad" />
+      <br />
+      <label>Nombre Festividad:</label>
+      <input v-model="Fiesta" />
+      <br>
+    </form>
+    <br>
+    <button v-on:click="crearTemp2">Crear Temporada</button>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+  name: "crear",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+    };
+  },
+  methods: {
+    crearTemp2: function () {
+ 
+axios.post('https://santi-hpsm.herokuapp.com/Temporada/crear/', {
+  
+  fecha_inicio: this.FechaIni,
+  fecha_fin: this.FechaFin,
+  ciudad: this.Ciudad,
+  fiesta: this.Fiesta
+})
+.then((response) => {
+  console.log(response);
+}, (error) => {
+  console.log(error);
+});
+
+    },
+  },
+};
+</script>
+<style>
+#Crear{
+margin-top: 30px;
+text-align: justify;
+margin-left: 500px;
+
+}
+</style>
---
+++ src/components/Mostrar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="mostrar_temp">
+      <h2>Temporadas Creadas</h2>
+      <table>
+
+
+      </table>
+    
+  </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+  name: "MostraTemp",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+    };
+  },
+  methods: {
+
+  },
+};
+</script>
+<style>
+#Crear{
+margin-top: 30px;
+text-align: justify;
+margin-left: 500px;
+
+}
+</style>
